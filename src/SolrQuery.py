0af9e559053589fe25a4d73ae211e12f29d513d1
chriskamphuis <mail@chriskamphuis.com>
2017-09-28 01:31:31 +0200

    Filter text on tuples

--- src/SolrQuery.py
+++ src/SolrQuery.py
@@ -1,5 +1,6 @@
 import pysolr
 import numpy as np
+import re
 
 def extract_fields(result):
     procedure = result['ProcedureType']                 # String
@@ -17,6 +18,28 @@ def main():
     sort = np.argsort(-c)
     for type, count in zip(un[sort], c[sort]):
         print("{0}: {1}".format(type, count))
+
+def get_article(input_sentence, wetboek):
+    info = {}
+    info['wetboek'] = wetboek
+    wetboek = wetboek.lower()
+    match = None
+    if wetboek == 'bw' or wetboek == 'burgelijk wetboek':
+        pattern = '([0-9]{1,2}a?A?):([0-9]{1,5}) (BW ?)?(Burgerlijk Wetboek ?)?(.{0,10}lid [0-9]{1,2})?'
+        match = re.search(pattern, input_sentence)
+        bwnummer, artikel, _, __, lid = match.groups()
+        info['bwnummer'] = bwnummer
+        info['artikel'] = artikel
+        info['lid'] = lid
+    elif wetboek == 'awb' or wetboek == 'algemene wet bestuursrecht':
+        pattern = 'artikel ?([0-9]{1,3}:[0-9]{1,3})(, )?(.{1,15}lid)?(.{1,20})?(Awb|Algemene wet bestuursrecht)'
+        match = re.search(pattern, input_sentence)
+        artikel, _, lid, __, wb = match.groups()
+        info['wetboek'] = wb
+        info['artikel'] = artikel
+        info['lid'] = lid
+        info['bwnummer'] = None
+    return info
 
 if __name__ == '__main__':
     main()
