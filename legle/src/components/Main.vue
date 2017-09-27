575154ba420ea84f62e919d25a3ea183ee402ce2
Guido Zuidhof <me@guido.io>
2017-09-27 15:06:35 +0200

    Initial view css/html framework

---
+++ legle/src/components/Main.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="main-container">
+    <graph-view class="abs fs"></graph-view>
+    <widget></widget>
+    <span class="footer-text">Legle - <span style="opacity: 0.6">legal Google blendle</span> </span>
+  </div>
+</template>
+
+<script>
+import Graph from './Graph.vue'
+import Widget from './Widget.vue'
+
+export default {
+  components: {
+    'graph-view': Graph,
+    'widget': Widget,
+  },
+  name: 'main',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.main-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.footer-text {
+  opacity: 0.65;
+  color: #343434;
+  position: absolute;
+  bottom: 4px;
+  right: 24px;
+  font-size: 1.18em;
+  transition: all cubic-bezier(0.19, 1, 0.22, 1) 500ms;
+}
+
+.footer-text:hover {
+  transform: scale(1.125);
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ legle/src/components/Widget.vue
@@ -0,0 +1,69 @@
+<template>
+<form class="sidebar">
+    <div class="widget widget-search">
+      <input class="search-input" placeholder="Enter an ECLI number.."></input>
+    </div>
+  <div class="widget widget-main">
+    <div class="widget-section">
+        <h3>Document details</h3>
+        
+    </div>
+  </div>
+  </form>
+</template>
+
+<script>
+import Graph from './Graph.vue'
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.sidebar {
+    display: flex;
+    flex-direction: column;
+    margin: 28px;
+    width: 360px;
+    position: absolute;
+}
+
+.widget {
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    background-color: #fdfdfd;
+    width: 360px;
+    border-radius: 7px;
+    box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.22);
+    color: #777;
+}
+
+.widget-main {
+    min-height: 420px;
+}
+
+.widget-search {
+    margin-bottom: 24px;
+}
+
+.search-input {
+    font-size: 1.45em;
+    border-style: none;
+    padding: 5px 8px;
+}
+
+.widget-section {
+    border-bottom: 2px solid #ddd;
+}
+
+.widget-section h3 {
+    font-weight: normal;
+}
+
+</style>
